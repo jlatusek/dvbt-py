7c1baaab54b2a61bb319cea4390ece021329b0b3
Jakub Latusek <jk.latusek@gmail.com>
2024-08-22 17:39:35 +0200

    Simplify tps tests
    * There is no need to add 1, because there are two different start sequence for next tps frame

--- tests/test_tps.py
+++ tests/test_tps.py
@@ -17,7 +17,8 @@ def symbols() -> np.ndarray[np.complex64]:
 def test_find_symbol(symbols):
     tps_bits = tps.extract_bit_data(symbols)
     frame1_idx = tps.find_frame_start(tps_bits, 0)
-    frame2_idx = tps.find_frame_start(tps_bits[frame1_idx + 1 :], 1)
-    frame3_idx = tps.find_frame_start(tps_bits[frame1_idx + frame2_idx + 2 :], 2)
-    assert frame2_idx + 1 == 68
-    assert frame3_idx + 1 == 68
+    frame2_idx = tps.find_frame_start(tps_bits[frame1_idx :], 1)
+    frame3_idx = tps.find_frame_start(tps_bits[frame1_idx + frame2_idx :], 2)
+    assert frame2_idx == 68
+    assert frame3_idx == 68
+
